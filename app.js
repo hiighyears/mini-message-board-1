2099787efe7959fe048f8404edb5c720beed89b4
Anam <121228616+hiighyears@users.noreply.github.com.>
2026-03-04 01:33:43 +0530

    construstes port

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ app.set("view engine", "ejs");
 const routes = require('./routes/routes')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+
 
 app.use('/',routes);
-app.listen(3000,'localhost',()=> {
+app.listen(PORT,()=> {
     console.log('server listening on 3000')
 });
